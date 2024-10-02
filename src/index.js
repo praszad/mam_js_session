9d3225ea7c3262597a85786085f834369c9736fb
Praszad <impraszad@gmail.com>
2024-10-02 22:36:26 +0530

    Master branch setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Counter from './Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <p>Hello</p>
   </React.StrictMode>
 );
 
